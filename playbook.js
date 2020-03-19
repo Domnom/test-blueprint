8c228aa58b0b49fd2a55cae465b15080cd854e6c
Dominic Trang <dom@kitset.io>
2020-03-20 11:48:38 +1300

    feat(playbook.js): Initialising the playbook js file that can be used to generate the playbook json file

---
+++ playbook.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+playbook("LOOK IVE CHANGED TODO - Get the name from github url")
+	.addCategory("TODO - Create a category name")
+		.addScene("TODO - Create a scene name")
+			.addStep("step-1")
+			.addStep("step-2")
+				.addCli()
+					.withCommand("git checkout -b feature/cool")
+				.withTime({
+					"start" : 0,
+					"duration" : 100
+				})
+			.addStep("step-3")
+				.addCli()
+					.withCommand("git checkout -b feature/cool-validation")
+				.withTime({
+					"start" : 0,
+					"duration" : 100
+				})
+			.addStep("step-4")
+				.addCli()
+					.withCommand("git checkout -b feature/cool")
+				.withTime({
+					"start" : 0,
+					"duration" : 100
+				})
+	.write(path.join(__dirname, "playbook.json"));
