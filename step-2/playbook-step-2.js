d6f716ce5cbe88b0417f3ec38fa754dbf8c0fb8f
Dominic Trang <dom@kitset.io>
2020-03-21 18:45:46 +1300

    feat(playbook-step-2.js): Initialising the playbook step js file which is used by the main playbook.js file

---
+++ step-2/playbook-step-2.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = step("step-2")
+					.addCode("/code/features/cool/CoolFeature.hbs", "features/cool/CoolFeature.js")
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
+					.addCli()
+						.withCommand("git checkout -b feature/cool")
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
