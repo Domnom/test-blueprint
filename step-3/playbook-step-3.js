f3ea3f12913b67d2ae165862b831872280ac0725
Dominic Trang <dom@kitset.io>
2020-03-21 18:45:46 +1300

    feat(playbook-step-3.js): Initialising the playbook step js file which is used by the main playbook.js file

---
+++ step-3/playbook-step-3.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = step("step-3")
+					.addCode("/code/features/cool/CoolFeature.hbs", "features/cool/CoolFeature.js")
+						.withPartial("partial_1", "step-3/code/features/cool/CoolFeature_partials/partial_1.hbs", 0, 100)
+						.withPartial("partial_2", "step-3/code/features/cool/CoolFeature_partials/partial_2.hbs", 100, 100)
+						.withPartial("partial_3", "step-3/code/features/cool/CoolFeature_partials/partial_3.hbs", 200, 100)
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
+					.addCode("/code/service/ValidationService.hbs", "service/ValidationService.js")
+					.withTime({
+						"start" : 100,
+						"duration" : 100
+					})
+					.addCli()
+						.withCommand("git checkout -b feature/cool-validation")
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
