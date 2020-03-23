15cda351b2c000b360223da2676893f38b22ef7c
Dominic Trang <dom@kitset.io>
2020-03-24 12:28:01 +1300

    feat(playbook-step-03.js): Initialising the playbook step js file which is used by the main playbook.js file

---
+++ step-03/playbook-step-03.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = step("step-03")
+					.addCode("/code/docker-compose.hbs", "docker-compose.yml")
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
+					.addCode("/code/backend/server/src/app.module.hbs", "backend/server/src/app.module.ts")
+						.withPartial("partial_1", "step-03/code/backend/server/src/app.module_partials/partial_1.hbs", 0, 100)
+						.withPartial("partial_2", "step-03/code/backend/server/src/app.module_partials/partial_2.hbs", 100, 100)
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
+					.addCli()
+						.withCommand("git checkout -b feature/docker-compose")
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
