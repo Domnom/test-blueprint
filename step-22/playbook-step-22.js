ebb81c8830caccbc98534acd8ab5611ee22863f4
Dominic Trang <dom@kitset.io>
2020-03-24 10:55:34 +1300

    feat(playbook-step-22.js): Initialising the playbook step js file which is used by the main playbook.js file

---
+++ step-22/playbook-step-22.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = step("step-22")
+					.addCode("/code/frontend/package.hbs", "frontend/package.json")
+						.withPartial("partial_1", "step-22/code/frontend/package_partials/partial_1.hbs", 0, 100)
+						.withPartial("partial_2", "step-22/code/frontend/package_partials/partial_2.hbs", 100, 100)
+					.withTime({
+						"start" : 0,
+						"duration" : 200
+					})
+					.addCode("/code/frontend/yarn.hbs", "frontend/yarn.lock")
+						.withPartial("partial_1", "step-22/code/frontend/yarn_partials/partial_1.hbs", 0, 100)
+						.withPartial("partial_2", "step-22/code/frontend/yarn_partials/partial_2.hbs", 100, 100)
+					.withTime({
+						"start" : 0,
+						"duration" : 16600
+					})
+					.addCode("/code/frontend/src/services/react-beautiful-dnd/responders.service.test.hbs", "frontend/src/services/react-beautiful-dnd/responders.service.test.ts")
+					.withTime({
+						"start" : 0,
+						"duration" : 1100
+					})
+					.addCode("/code/frontend/src/services/react-beautiful-dnd/responders.service.hbs", "frontend/src/services/react-beautiful-dnd/responders.service.ts")
+					.withTime({
+						"start" : 0,
+						"duration" : 500
+					})
+					.addCli()
+						.withCommand("git checkout -b feature/frontend/services/react-beautiful-dnd")
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
