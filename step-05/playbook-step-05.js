4d9e30990191b2551c91a2180904b1f0bb6e3625
Dominic Trang <dom@kitset.io>
2020-03-24 12:28:07 +1300

    feat(playbook-step-05.js): Initialising the playbook step js file which is used by the main playbook.js file

---
+++ step-05/playbook-step-05.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = step("step-05")
+					.addCode("/code/setup/jest.hbs", "setup/jest.sh")
+						.withPartial("partial_1", "step-05/code/setup/jest_partials/partial_1.hbs", 0, 100)
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
+					.addCli()
+						.withCommand("git checkout -b feature/setup/jest-and-docker")
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
