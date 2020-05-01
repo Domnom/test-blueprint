303d2a40e7e8ef6d8c739426efdd5cfcf8ffc2be
Dominic Trang <dom@kitset.io>
2020-05-01 16:41:02 +1200

    feat(playbook-step-04.js): Initialising the playbook step js file which is used by the main playbook.js file

---
+++ step-04/playbook-step-04.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = step("step-04")
+					.addCode("step-04/code/features/cool/CoolFeature.hbs", "features/cool/CoolFeature.js")
+						.withPartial("partial_1", "step-04/code/features/cool/CoolFeature_partials/partial_1.hbs", 0, 100)
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
+					.addCli()
+						.withCommand("git checkout -b feature/cool and it was modified")
+					.withTime({
+						"start" : 0,
+						"duration" : 100
+					})
