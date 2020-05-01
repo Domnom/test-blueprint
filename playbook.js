aad6381f4ef9ce8e3e5639d68ca3181fbbd1a0a9
Dominic Trang <dom@kitset.io>
2020-05-01 17:40:43 +1200

    feat(playbook.js): Initialising the playbook js file that can be used to generate the playbook json file

---
+++ playbook.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const step_01 = require('./step-01/playbook-step-01.js');
+const step_02 = require('./step-02/playbook-step-02.js');
+const step_03 = require('./step-03/playbook-step-03.js');
+const step_04 = require('./step-04/playbook-step-04.js');
+const trailing_commits = require('./trailing-commits/playbook-trailing-commits.js');
+
+playbook("LOOK IVE CHANGED TODO - Get the name from github url")
+	.addCategory("TODO - Create a category name")
+		.addScene("TODO - Create a scene name")
+			.addStepFromModel(step_01)
+			.addStepFromModel(step_02)
+			.addStepFromModel(step_03)
+			.addStepFromModel(step_04)
+			.addStepFromModel(trailing_commits)
+	.write(path.join(__dirname, "playbook.json"));
